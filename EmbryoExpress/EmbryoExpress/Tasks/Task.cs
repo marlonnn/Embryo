b8d819a8ba9440fde42f57688f536b82895959d3
Vincent <zhongw_1988@126.com>
2019-05-03 22:35:32 +0800

    修改任务类

--- EmbryoExpress/EmbryoExpress/Tasks/Task.cs
+++ EmbryoExpress/EmbryoExpress/Tasks/Task.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmbryoExpress
+namespace EmbryoExpress.Tasks
 {
     public enum Priority
     {
---
+++ EmbryoExpress/EmbryoExpress/Tasks/TaskManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmbryoExpress.Tasks
+{
+    public class TaskManager
+    {
+        List<Task> taskList;
+
+        public TaskManager()
+        {
+            taskList = new List<Task>();
+        }
+    }
+}
